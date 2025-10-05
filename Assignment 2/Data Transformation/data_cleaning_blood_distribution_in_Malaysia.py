99ebb6eab6f122aeaddeb1418702a82f52580d32
syeo0037 <syeo0037@student.monash.edu>
2025-10-05 18:26:39 +0800

    Updated cleaned datasets

--- Assignment 2/Data Transformation/data_cleaning_blood_distribution_in_Malaysia.py
+++ Assignment 2/Data Transformation/data_cleaning_blood_distribution_in_Malaysia.py
@@ -11,15 +11,19 @@ def get_Malaysia_blood_distribution():
 
     result = pd.DataFrame(
         {
-            "O_Total": df["BloodType_OPosPctOfPop_pct_YearFree"].values
+            "O": df["BloodType_OPosPctOfPop_pct_YearFree"].values
             + df["BloodType_ONegPctOfPop_pct_YearFree"].values,
-            "A_Total": df["BloodType_APosPctOfPop_pct_YearFree"].values
+            "A": df["BloodType_APosPctOfPop_pct_YearFree"].values
             + df["BloodType_ANegPctOfPop_pct_YearFree"].values,
-            "B_Total": df["BloodType_BPosPctOfPop_pct_YearFree"].values
+            "B": df["BloodType_BPosPctOfPop_pct_YearFree"].values
             + df["BloodType_BNegPctOfPop_pct_YearFree"].values,
-            "AB_Total": df["BloodType_ABPosPctOfPop_pct_YearFree"].values
+            "AB": df["BloodType_ABPosPctOfPop_pct_YearFree"].values
             + df["BloodType_ABNegPctOfPop_pct_YearFree"].values,
         }
+    )
+
+    result = result.melt(
+        var_name="Blood Type", value_name="Percentage"
     )
 
     result.to_csv(
